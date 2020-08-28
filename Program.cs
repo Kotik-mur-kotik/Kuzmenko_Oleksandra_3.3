de73c7f2d64f0c3ab65ff746b76e6f257342899c
Kotik-mur-kotik <Hacta14091993@gmail.com>
2020-08-28 15:46:08 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace laba3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = rand.Next(-100,100);
+                Console.Write("{0,4}", numbers[i]);
+            }
+            Console.WriteLine();
+            Console.Write("Enter the number ");
+            int variable = int.Parse(Console.ReadLine());
+            var selectedNumbers = from x in numbers
+                                  where x % 10 == variable && x > 0
+                                  select x;
+            variable = (selectedNumbers.Count() > 0) ? selectedNumbers.FirstOrDefault() : 0;
+            Console.WriteLine($"Your number is {variable}");
+        }
+    }
+}
